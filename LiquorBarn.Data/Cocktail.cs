69b6742a759543e4f7902814982d43f3ab956319
Gabriel Alvarez <galvarez2238@gmail.com>
2020-09-03 13:25:31 -0700

    added entities

--- LiquorBarn.Data/Cocktail.cs
+++ LiquorBarn.Data/Cocktail.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,13 @@ namespace LiquorBarn.Data
 {
     public class Cocktail
     {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        //public List<Liquor> LiquorsInCocktail { get; set; }
+
+        public string Ingredients { get; set; }
     }
 }
--- LiquorBarn.Data/Junction.cs
+++ LiquorBarn.Data/Junction.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,15 @@ namespace LiquorBarn.Data
 {
     public class Junction
     {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(Liquor))]
+        public int LiquorId { get; set; }
+        public virtual Liquor Liquor { get; set; }
+
+        [ForeignKey(nameof(Cocktail))]
+        public int CocktailId { get; set; }
+        public virtual Cocktail Cocktail { get; set; }
     }
 }
